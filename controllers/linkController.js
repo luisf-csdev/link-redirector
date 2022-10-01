72a1aecc02ea06e3bdbf70093e33c51180e54c80
Luís Felipe <luisf.csdev@gmail.com>
2022-10-01 20:36:58 -0300

    show registered links

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -44,6 +44,72 @@
 
 
 //INTEGRATING WITH FRONTEND
+// const Link = require('../models/Link');
+
+// const redirect = async (req, res) => {
+//     let title = req.params.title;
+//     try {
+//         let doc = await Link.findOne({ title });
+//         res.redirect(doc.url);
+//     } catch (error) {
+//         res.send(`There was an error: ${error}`);
+//     }
+// }
+
+// const addLink = async (req, res) => {
+
+//     let link = new Link(req.body);
+
+//     try {
+//         let doc = await link.save();
+//         res.send("Link successfully added!");
+//     } catch (error) {
+//         res.render('index', { error, body: req.body });
+//     }
+
+// }
+
+// module.exports = { redirect, addLink };
+
+
+//SEARCHING ALL LINKS
+// const Link = require('../models/Link');
+
+// const redirect = async (req, res) => {
+//     let title = req.params.title;
+//     try {
+//         let doc = await Link.findOne({ title });
+//         res.redirect(doc.url);
+//     } catch (error) {
+//         res.send(`There was an error: ${error}`);
+//     }
+// }
+
+// const addLink = async (req, res) => {
+
+//     let link = new Link(req.body);
+
+//     try {
+//         let doc = await link.save();
+//         res.send("Link successfully added!");
+//     } catch (error) {
+//         res.render('index', { error, body: req.body });
+//     }
+// }
+
+// const allLinks = async (req, res) => {
+//     try {
+//         let links = await Link.find({});
+//         res.send(links);
+//     } catch (error) {
+//         res.send(error);
+//     }
+// }
+
+// module.exports = { redirect, addLink, allLinks };
+
+
+//SHOWING LINKS ON SCREEN
 const Link = require('../models/Link');
 
 const redirect = async (req, res) => {
@@ -66,7 +132,15 @@ const addLink = async (req, res) => {
     } catch (error) {
         res.render('index', { error, body: req.body });
     }
-
 }
 
-module.exports = { redirect, addLink };
+const allLinks = async (req, res) => {
+    try {
+        let links = await Link.find({});
+        res.render('all', { links });
+    } catch (error) {
+        res.send(error);
+    }
+}
+
+module.exports = { redirect, addLink, allLinks };
--- routes/linkRoute.js
+++ routes/linkRoute.js
@@ -25,9 +25,25 @@
 
 
 //INTEGRATING WITH FRONTEND
+// const express = require('express');
+// const router = express.Router();
+// const linkController = require('../controllers/linkController')
+
+// router.get('/:title', linkController.redirect)
+
+// router.get('/', (req, res) => res.render('index', { error: false, body: {} }));
+
+// router.post('/', express.urlencoded({ extended: true }), linkController.addLink);
+
+// module.exports = router;
+
+
+//SEARCHING ALL LINKS
 const express = require('express');
 const router = express.Router();
 const linkController = require('../controllers/linkController')
+
+router.get('/all', linkController.allLinks);
 
 router.get('/:title', linkController.redirect)
 

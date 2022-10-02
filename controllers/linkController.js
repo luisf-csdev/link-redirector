33f9fab14e8f5e7a8abc4c2b0ddda5a6864a398d
Luís Felipe <luisf.csdev@gmail.com>
2022-10-02 01:25:47 -0300

    adjusting routes

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -110,6 +110,198 @@
 
 
 //SHOWING LINKS ON SCREEN
+// const Link = require('../models/Link');
+
+// const redirect = async (req, res) => {
+//     let title = req.params.title;
+//     try {
+//         let doc = await Link.findOne({ title });
+//         res.redirect(doc.url);
+//     } catch (error) {
+//         res.send(`There was an error: ${error}`);
+//     }
+// }
+
+// const addLink = async (req, res) => {
+
+//     let link = new Link(req.body);
+
+//     try {
+//         let doc = await link.save();
+//         res.send("Link successfully added!");
+//     } catch (error) {
+//         res.render('index', { error, body: req.body });
+//     }
+// }
+
+// const allLinks = async (req, res) => {
+//     try {
+//         let links = await Link.find({});
+//         res.render('all', { links });
+//     } catch (error) {
+//         res.send(error);
+//     }
+// }
+
+// module.exports = { redirect, addLink, allLinks, deleteLink };
+
+
+//DELETING A DOC
+// const Link = require('../models/Link');
+
+// const redirect = async (req, res) => {
+//     let title = req.params.title;
+//     try {
+//         let doc = await Link.findOne({ title });
+//         res.redirect(doc.url);
+//     } catch (error) {
+//         res.send(`There was an error: ${error}`);
+//     }
+// }
+
+// const addLink = async (req, res) => {
+
+//     let link = new Link(req.body);
+
+//     try {
+//         let doc = await link.save();
+//         res.send("Link successfully added!");
+//     } catch (error) {
+//         res.render('index', { error, body: req.body });
+//     }
+// }
+
+// const allLinks = async (req, res) => {
+//     try {
+//         let links = await Link.find({});
+//         res.render('all', { links });
+//     } catch (error) {
+//         res.send(error);
+//     }
+// }
+
+// const deleteLink = async (req, res) => {
+
+//     let id = req.params.id;
+//     if (!id) {
+//         id = req.body.id;
+//     }
+
+//     try {
+//         res.send(await Link.findByIdAndDelete(id));
+//     } catch (error) {
+//         res.send(error);
+//     }
+// }
+
+// module.exports = { redirect, addLink, allLinks, deleteLink };
+
+
+//DELETING A DOC -  FRONTEND
+// const Link = require('../models/Link');
+
+// const redirect = async (req, res) => {
+//     let title = req.params.title;
+//     try {
+//         let doc = await Link.findOne({ title });
+//         res.redirect(doc.url);
+//     } catch (error) {
+//         res.send(`There was an error: ${error}`);
+//     }
+// }
+
+// const addLink = async (req, res) => {
+
+//     let link = new Link(req.body);
+
+//     try {
+//         let doc = await link.save();
+//         res.send("Link successfully added!");
+//     } catch (error) {
+//         res.render('index', { error, body: req.body });
+//     }
+// }
+
+// const allLinks = async (req, res) => {
+//     try {
+//         let links = await Link.find({});
+//         res.render('all', { links });
+//     } catch (error) {
+//         res.send(error);
+//     }
+// }
+
+// const deleteLink = async (req, res) => {
+
+//     let id = req.params.id;
+//     if (!id) {
+//         id = req.body.id;
+//     }
+
+//     try {
+//         await Link.findByIdAndDelete(id);
+//         res.send(id);
+//     } catch (error) {
+//         res.status(404).send(error);
+//     }
+// }
+
+// module.exports = { redirect, addLink, allLinks, deleteLink };
+
+
+//ANOTHER WAY TO DELETE - FORM METHOD OVERRIDE
+// const Link = require('../models/Link');
+
+// const redirect = async (req, res) => {
+//     let title = req.params.title;
+//     try {
+//         let doc = await Link.findOne({ title });
+//         res.redirect(doc.url);
+//     } catch (error) {
+//         res.send(`There was an error: ${error}`);
+//     }
+// }
+
+// const addLink = async (req, res) => {
+
+//     let link = new Link(req.body);
+
+//     try {
+//         let doc = await link.save();
+//         res.send("Link successfully added!");
+//     } catch (error) {
+//         res.render('index', { error, body: req.body });
+//     }
+// }
+
+// const allLinks = async (req, res) => {
+//     try {
+//         let links = await Link.find({});
+//         res.render('all', { links });
+//     } catch (error) {
+//         res.send(error);
+//     }
+// }
+
+// const deleteLink = async (req, res) => {
+
+//     let id = req.params.id;
+//     if (!id) {
+//         id = req.body.id;
+//     }
+
+//     try {
+//         await Link.findByIdAndDelete(id);
+//         res.redirect('/all');
+//     } catch (error) {
+//         res.status(404).send(error);
+//     }
+// }
+
+// module.exports = { redirect, addLink, allLinks, deleteLink };
+
+
+//ADJUSTING ROUTES
 const Link = require('../models/Link');
 
 const redirect = async (req, res) => {
@@ -128,7 +320,7 @@ const addLink = async (req, res) => {
 
     try {
         let doc = await link.save();
-        res.send("Link successfully added!");
+        res.redirect('/');
     } catch (error) {
         res.render('index', { error, body: req.body });
     }
@@ -143,4 +335,19 @@ const allLinks = async (req, res) => {
     }
 }
 
-module.exports = { redirect, addLink, allLinks };
+const deleteLink = async (req, res) => {
+
+    let id = req.params.id;
+    if (!id) {
+        id = req.body.id;
+    }
+
+    try {
+        await Link.findByIdAndDelete(id);
+        res.redirect('/');
+    } catch (error) {
+        res.status(404).send(error);
+    }
+}
+
+module.exports = { redirect, addLink, allLinks, deleteLink };
--- routes/linkRoute.js
+++ routes/linkRoute.js
@@ -39,16 +39,102 @@
 
 
 //SEARCHING ALL LINKS
+// const express = require('express');
+// const router = express.Router();
+// const linkController = require('../controllers/linkController')
+
+// router.get('/all', linkController.allLinks);
+
+// router.get('/:title', linkController.redirect)
+
+// router.get('/', (req, res) => res.render('index', { error: false, body: {} }));
+
+// router.post('/', express.urlencoded({ extended: true }), linkController.addLink);
+
+// module.exports = router;
+
+
+//DELETING A DOC
+// const express = require('express');
+// const router = express.Router();
+// const linkController = require('../controllers/linkController')
+
+// router.get('/all', linkController.allLinks);
+
+// router.get('/:title', linkController.redirect)
+
+// router.get('/', (req, res) => res.render('index', { error: false, body: {} }));
+
+// router.post('/', express.urlencoded({ extended: true }), linkController.addLink);
+
+// router.delete('/:id', linkController.deleteLink);
+
+// router.delete('/', express.urlencoded({ extended: true }), linkController.deleteLink);
+
+// module.exports = router;
+
+
+//DELETING A DOC - FRONTEND
+// const express = require('express');
+// const router = express.Router();
+// const linkController = require('../controllers/linkController')
+
+// router.get('/all', linkController.allLinks);
+
+// router.get('/:title', linkController.redirect)
+
+// router.get('/', (req, res) => res.render('index', { error: false, body: {} }));
+
+// router.post('/', express.urlencoded({ extended: true }), linkController.addLink);
+
+// router.delete('/:id', linkController.deleteLink);
+
+// router.delete('/', express.json(), linkController.deleteLink);
+
+// module.exports = router;
+
+
+//ANOTHER WAY TO DELETE - FORM METHOD OVERRIDE
+// const express = require('express');
+// const router = express.Router();
+// const linkController = require('../controllers/linkController');
+// var methodOverride = require('method-override');
+
+// router.use(methodOverride('_method'));
+
+// router.get('/all', linkController.allLinks);
+
+// router.get('/:title', linkController.redirect)
+
+// router.get('/', (req, res) => res.render('index', { error: false, body: {} }));
+
+// router.post('/', express.urlencoded({ extended: true }), linkController.addLink);
+
+// router.delete('/:id', linkController.deleteLink);
+
+// router.delete('/', express.urlencoded({ extended: true }), linkController.deleteLink);
+
+// module.exports = router;
+
+
+//ADJUSTING THE ROUTES
 const express = require('express');
 const router = express.Router();
-const linkController = require('../controllers/linkController')
+const linkController = require('../controllers/linkController');
+var methodOverride = require('method-override');
 
-router.get('/all', linkController.allLinks);
+router.use(methodOverride('_method'));
+
+router.get('/', linkController.allLinks);
+
+router.get('/add', (req, res) => res.render('index', { error: false, body: {} }));
 
 router.get('/:title', linkController.redirect)
 
-router.get('/', (req, res) => res.render('index', { error: false, body: {} }));
-
 router.post('/', express.urlencoded({ extended: true }), linkController.addLink);
+
+router.delete('/:id', linkController.deleteLink);
+
+router.delete('/', express.urlencoded({ extended: true }), linkController.deleteLink);
 
 module.exports = router;
